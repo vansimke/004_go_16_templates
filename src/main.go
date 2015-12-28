2c430717cdd97505b2369f65e99b3dde2e810182
Michael Van Sickle <mvansickle1@gmail.com>
2015-12-27 21:01:28 -0500

    after recording

--- src/main.go
+++ src/main.go
@@ -1,13 +1,47 @@
 package main
 
 import (
+	"html/template"
+	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 func main() {
+	templates := parseTemplates()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
+		templates["index.html"].Execute(w, struct{ Title string }{Title: "Index"})
+	})
+
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		templates["about.html"].Execute(w, struct{ Title string }{Title: "About"})
 	})
 
 	http.ListenAndServe(":3000", nil)
+}
+
+func parseTemplates() map[string]*template.Template {
+	result := make(map[string]*template.Template)
+
+	layout, _ := template.ParseFiles("templates/_layout.html")
+
+	dir, _ := os.Open("templates/blocks")
+	defer dir.Close()
+
+	fis, _ := dir.Readdir(-1)
+
+	for _, fi := range fis {
+		if fi.IsDir() {
+			continue
+		}
+		f, _ := os.Open("templates/blocks/" + fi.Name())
+		content, _ := ioutil.ReadAll(f)
+		f.Close()
+		tmpl, _ := layout.Clone()
+		tmpl.Parse(string(content))
+		result[fi.Name()] = tmpl
+	}
+
+	return result
 }
